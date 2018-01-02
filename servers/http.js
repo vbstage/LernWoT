dcd82649e954e10a8b8979e246c0951bf6108d91
Vladimir Boris <vladimir.boris.bel@gmail.com>
2018-01-02 18:51:13 +0300

    request console log

--- servers/http.js
+++ servers/http.js
@@ -8,6 +8,18 @@ var app = express();
 
 app.use(cors());
 
+app.use(function(req, res, next){
+   var date = new Date();
+   console.log( date.getHours() + ':' +
+                date.getMinutes() + ':' +
+                date.getSeconds() +
+                ' got ' + req.method + ' ' +
+                req.originalUrl +' Request from: ' +
+                req.hostname + ' ' +
+                req.ip + ' ');
+
+   next();
+});
 app.use('/pi/actuators', actuatorsRoutes);
 app.use('/pi/sensors', sensorRoutes);
 
