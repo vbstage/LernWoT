878294b90b350257e09b9769f097432fbd78a327
Vladimir Boris <vladimir.boris.bel@gmail.com>
2018-01-03 14:57:58 +0300

    *

--- wot-server.js
+++ wot-server.js
@@ -8,8 +8,8 @@ var wot = {
             pirPlugin = require('./plugins/internal/pirPlugin'),
             dhtPlugin = require('./plugins/internal/DHT22SensorPlugin');
 
-        pirPlugin.start({'sumilate': true, 'frequency': 2000});
-        dhtPlugin.start({'sumilate': false, 'frequency': 5000});
+        pirPlugin.start({'simulate': true, 'frequency': 3000});
+        dhtPlugin.start({'simulate': false, 'frequency': 5000});
 
         var server = httpServer.listen(resources.pi.port, function(){
             console.info('Your WoT Pi is up and running on port %s',
