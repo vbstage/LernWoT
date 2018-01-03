15dc244e4d8b23dc7967a6300fceb339f073c6f3
Vladimir Boris <vladimir.boris.bel@gmail.com>
2018-01-03 19:02:25 +0300

    Converter Refactoring

--- middleware/converter.js
+++ middleware/converter.js
@@ -2,40 +2,33 @@ var msgpack = require('msgpack5')(),
     encode = msgpack.encode,
     json2html = require('node-json2html');
 
-module.exports = function() {
-  return function (req, res, next) {
-      console.info('Representation converter middleware called');
+module.exports = function () {
+    return function (req, res, next) {
+        console.info('Representation converter middleware called');
 
-      if(req.result) {
-          if(req.accepts('json')) {
-              console.info('JSON representation selected!');
+        if (req.result) {
+            switch (req.accepts(['json', 'html', 'application/x-msgpack'])) {
+                case 'html':
+                    console.info('HTML representation selected!');
 
-              res.send(req.result);
-              return;
-          }
+                    var transform = {'tag': 'div', 'html': '${name} : ${value}'};
 
-          if(req.accepts('html')) {
-              console.info('HTML representation selected!');
+                    res.send(json2html.transform(req.result, transform));
+                    return;
+                case 'application/x-msgpack':
+                    console.info('MessagePack representation selected!');
 
-              var transform = {'tag': 'div', 'html': '${name} : ${value}'};
-
-              res.send(json2html.transform(req.result, transform));
-              return;
-          }
-
-          if(req.accepts('application/x-msgpack')) {
-              console.info('MessagePack representation selected!');
-
-              res.type('application/x-msgpack');
-              res.send(encode(req.result));
-              return;
-          }
-
-          console.info('Dafaulting to JSON representation');
-          res.send(req.result);
-          return;
-      } else {
-          next();
-      }
-  }
+                    res.type('application/x-msgpack');
+                    res.send(encode(req.result));
+                    return;
+                default:
+                    console.info('Dafaulting to JSON representation');
+                    res.send(req.result);
+                    return;
+            }
+        }
+        else {
+            next();
+        }
+    }
 };
